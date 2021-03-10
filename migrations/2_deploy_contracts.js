35492f9669bec2d2812597828071cc48aa023ea0
Milan <62163451+Milan-99@users.noreply.github.com>
2021-03-10 17:02:42 +0545

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
-var tfuel = artifacts.require("./tfuel.sol");
-var thetha = artifacts.require("./thetha.sol");
+var ybet = artifacts.require("./tfuel.sol");
+var xbet = artifacts.require("./thetha.sol");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(tfuel, "XBET", "XBET", 0, 0);
-  const xbet = await tfuel.deployed();
+  await deployer.deploy(ybet, "YBET", "YBET", 0, 0);
+  const y = await ybet.deployed();
 
-  await deployer.deploy(thetha, "YBET", "YBET", 1000000, 0, xbet.address);
-  const ybet = await thetha.deployed();
+  await deployer.deploy(XBET, "xBET", "XBET", 1000000, 0, y.address);
+  const x = await thetha.deployed();
 
-  xbet.transferowner(ybet.address);
-  ybet.transfer("0x5961B58E4B5f9589C28b384e4b42705f5DCc701F", 10000);
+  y.transferowner(x.address);
+  
 };
